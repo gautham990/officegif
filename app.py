20056fee1e106cae0bdb12310df3350b0ce9273f
gautham990 <gautham990@gmail.com>
2023-12-29 23:34:00 +0530

    Added hostname feature

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
+import socket
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    hostname = socket.gethostname()
     message = os.getenv('MESSAGE', 'This is default message')
     secretcode = os.getenv('SECRETCODE', 'No secet code')
-    return render_template('index.html', message=message, secretcode=secretcode)
+    return render_template('index.html', message=message, secretcode=secretcode, hostname=hostname)
 if __name__ == '__main__':
     app.run(debug=True)
